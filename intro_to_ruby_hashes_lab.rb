bf60846afddb03ee17d687d315e41489e3858ae2
Alex Foreman <foreman.alexander@gmail.com>
2020-07-06 16:18:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,9 +6,9 @@ end
 def monopoly_with_second_tier
   { :railroads => 
     {:pieces => 4},
-    {:rent_in_dollars => {} }
+    {:rent_in_dollars => {} 
+  }
 }
-
 end
 
 def monopoly_with_third_tier
